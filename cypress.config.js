aa36366dedc7f1921500c14e59eb87c23bb9c847
Sérgio Dornelas <110427773+sergiodornelas@users.noreply.github.com>
2025-09-19 20:30:57 -0300

    setup

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
-export default {
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-};
+});
