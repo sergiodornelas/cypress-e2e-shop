a55cad807c8f08f4a3b8123245daf7c95e229e8c
Sérgio Dornelas <110427773+sergiodornelas@users.noreply.github.com>
2025-09-19 22:15:33 -0300

    ts

--- cypress/e2e/authenticate/login.cy.ts
+++ cypress/e2e/authenticate/login.cy.ts
@@ -4,7 +4,6 @@ beforeEach(() => {
   cy.visit("/minha-conta/");
 });
 
-
 describe('login', () => {
   const users = Cypress.env('users');
 
@@ -13,5 +12,30 @@ describe('login', () => {
     cy.get('[id="password"]').type(users[1].password, {log:false})
     cy.get('input[type="submit"][name="login"]').click()
   });
+
+  it('login with invalid credentials', () => {
+    cy.get('[id="username"]').type(users[0].email, {log:false}), 
+    cy.get('[id="password"]').type(users[0].password, {log:false})
+    cy.get('input[type="submit"][name="login"]').click()
+  }); 
+
+  it('login with empty credentials', () => {
+    cy.get('[id="username"]')
+    cy.get('[id="password"]')
+    cy.get('input[type="submit"][name="login"]').click()
+  });
+
+  it('login with valid email and invalid password', () => {
+    cy.get('[id="username"]').type(users[1].email, {log:false}), 
+    cy.get('[id="password"]').type(users[0].password, {log:false})
+    cy.get('input[type="submit"][name="login"]').click()
+  });
+
+it('login with invalid email and valid password', () => {
+    cy.get('[id="username"]').type(users[0].email, {log:false}), 
+    cy.get('[id="password"]').type(users[1].password, {log:false})
+    cy.get('input[type="submit"][name="login"]').click()
+  });
+
 });
 
