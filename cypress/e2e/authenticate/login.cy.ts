c38ddc035d515cf14163e87d27b32d86608172ed
Sérgio Dornelas <110427773+sergiodornelas@users.noreply.github.com>
2025-09-21 20:35:38 -0300

    Update login.cy.ts

--- cypress/e2e/authenticate/login.cy.ts
+++ cypress/e2e/authenticate/login.cy.ts
@@ -7,31 +7,31 @@ beforeEach(() => {
 describe('login', () => {
   const users = Cypress.env('users');
 
-  it('login with valid credentials', () => {
+  it('1. login with valid credentials.', () => {
     cy.get('[id="username"]').type(users[1].email, {log:false}), 
     cy.get('[id="password"]').type(users[1].password, {log:false})
     cy.get('input[type="submit"][name="login"]').click()
   });
 
-  it('login with invalid credentials', () => {
+  it('2. login with invalid credentials.', () => {
     cy.get('[id="username"]').type(users[0].email, {log:false}), 
     cy.get('[id="password"]').type(users[0].password, {log:false})
     cy.get('input[type="submit"][name="login"]').click()
   }); 
 
-  it('login with empty credentials', () => {
+  it('3. login with empty credentials.', () => {
     cy.get('[id="username"]')
     cy.get('[id="password"]')
     cy.get('input[type="submit"][name="login"]').click()
   });
 
-  it('login with valid email and invalid password', () => {
+  it('4. login with valid email and invalid password.', () => {
     cy.get('[id="username"]').type(users[1].email, {log:false}), 
     cy.get('[id="password"]').type(users[0].password, {log:false})
     cy.get('input[type="submit"][name="login"]').click()
   });
 
-it('login with invalid email and valid password', () => {
+it('5. login with invalid email and valid password.', () => {
     cy.get('[id="username"]').type(users[0].email, {log:false}), 
     cy.get('[id="password"]').type(users[1].password, {log:false})
     cy.get('input[type="submit"][name="login"]').click()
