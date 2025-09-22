85d748db3d226b16186da99b7f9122bf555bb355
Sérgio Dornelas <110427773+sergiodornelas@users.noreply.github.com>
2025-09-22 09:09:08 -0300

    cy

--- cypress/support/commands/login.ts
+++ cypress/support/commands/login.ts
@@ -4,4 +4,3 @@ Cypress.Commands.add('LoginDataValid', (email: string, password: string) => {
     cy.get('[id="password"]').type(password)
     cy.get('input[type="submit"][name="login"]').click()
 })
-
