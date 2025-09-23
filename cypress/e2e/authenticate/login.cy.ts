d48c8e788b51e491bd1ac86ef928db6cc5f8f3f9
Sérgio Dornelas <110427773+sergiodornelas@users.noreply.github.com>
2025-09-23 13:14:23 -0300

    cy

--- cypress/e2e/authenticate/login.cy.ts
+++ cypress/e2e/authenticate/login.cy.ts
@@ -1,20 +1,22 @@
 /// <reference types="cypress" />
+import cypress = require('cypress');
 import '../../support/commands'
+const authValid = Cypress.env('validUsers');
+const authInvalid = Cypress.env('invalidUsers');
 
 beforeEach(() => {
   cy.visit("/minha-conta/");
 });
 
 describe('login', () => {
-  const users = Cypress.env('users');
 
   it('1. login with valid credentials.', () => {
-    cy.LoginDataValid(users[1].email, users[1].password)
+    cy.LoginDataValid(Cypress.env(authValid).email, Cypress.env(authValid).password)
   });
 
   it('2. login with invalid credentials.', () => {
-    cy.get('[id="username"]').type(users[0].email, { log: false }),
-    cy.get('[id="password"]').type(users[0].password, { log: false })
+    cy.get('[id="username"]').type(Cypress.env(authInvalid).email, { log: false }),
+    cy.get('[id="password"]').type(Cypress.env(authInvalid).password, { log: false })
     cy.get('input[type="submit"][name="login"]').click()
   });
 
@@ -25,16 +27,13 @@ describe('login', () => {
   });
 
   it('4. login with valid email and invalid password.', () => {
-    cy.get('[id="username"]').type(users[1].email, { log: false }),
-    cy.get('[id="password"]').type(users[0].password, { log: false })
+    cy.get('[id="username"]').type(Cypress.env(authValid).email, { log: false }),
+    cy.get('[id="password"]').type(Cypress.env(authInvalid).password, { log: false })
     cy.get('input[type="submit"][name="login"]').click()
   });
 
   it('5. login with invalid email and valid password.', () => {
-    cy.get('[id="username"]').type(users[0].email, { log: false }),
-    cy.get('[id="password"]').type(users[1].password, { log: false })
-    cy.get('input[type="submit"][name="login"]').click()
-  });
-
+    cy.get('[id="username"]').type(Cypress.env(authInvalid).email, { log: false }),
+    cy.get('[id="password"]').type(Cypress.env(authValid).password, { log: false })
+}); 
 });
-
