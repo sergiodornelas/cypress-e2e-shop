decb3a2f7a4ba8621f66143c80e699d753078b00
Sérgio Dornelas <110427773+sergiodornelas@users.noreply.github.com>
2025-12-30 09:03:59 -0300

    improvements

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,5 @@
 import "../support/commands";
-import users from '../fixtures/login.json'
+import { faker } from '@faker-js/faker';
 
 beforeEach(() => {
   cy.clearCookies();
@@ -8,39 +8,47 @@ beforeEach(() => {
 });
 
 describe("login", () => {
-  const {EMAIL, PASSWORD} = Cypress.env('USER')
-  const {INVALID_EMAIL, INVALID_PASSWORD} =  users.DATA_INVALID
+  const {EMAIL, PASSWORD} = Cypress.env('USER');
+  const fakeEmail = faker.internet.email();
+  const fakePassword = faker.internet.password();
 
-  it("1. should login successfully with valid credentials", () => {
-    cy.fillForm(EMAIL, PASSWORD, { log: false });
+it("1. should login successfully with valid credentials", () => {
+    cy.fillForm(EMAIL, PASSWORD);
+    cy.submitLogin();
     cy.get("h1.page-title")
       .should("be.visible")
       .and("contain.text", "Minha conta");
   });
 
   it("2. should not login with completely invalid credentials", () => {
-    cy.fillForm(INVALID_EMAIL, INVALID_PASSWORD, { log: false });
+    cy.fillForm(fakeEmail, fakePassword);
+    cy.submitLogin();
     cy.contains("Endereço de e-mail desconhecido.")
       .should("be.visible");
   });
 
   it("3. should not login with empty credentials", () => {
-    cy.emptyForm();
+    cy.submitLogin();
     cy.contains("Nome de usuário é obrigatório")
       .should("be.visible");
   });
 
   it("4. should not login with valid email and invalid password", () => {
-    cy.fillForm(EMAIL, INVALID_PASSWORD, { log: false });
+    cy.fillForm(EMAIL, fakePassword);
+    cy.submitLogin();
     cy.contains("Perdeu a senha?")
       .should("be.visible");
   });
 
   it("5. should not login with invalid email and valid password", () => {
-    cy.fillForm(INVALID_EMAIL, PASSWORD, { log: false });
+    cy.fillForm(fakeEmail, PASSWORD);
+    cy.submitLogin();
     cy.contains("Verifique novamente ou tente seu nome de usuário.")
       .should("be.visible");
   });
-});
+})
+
+  
+
 
 //npm install --save-dev mochawesome mochawesome-merge mochawesome-report-generator
--- cypress/support/commands/login.js
+++ cypress/support/commands/login.js
@@ -1,9 +1,19 @@
-Cypress.Commands.add('fillForm', (email, password, options = {}) => {
-  cy.get('[id="username"]').type(email, { log: options.log ?? true });
-  cy.get('[id="password"]').type(password, { log: options.log ?? true });
+
+Cypress.Commands.add('fillEmail', (email) => {
+  cy.get('[id="username"]').type(email);
+
+})
+
+Cypress.Commands.add('fillPassword', (password) => {
+  cy.get('[id="password"]').type(password);
+
+})
+
+Cypress.Commands.add('submitLogin', () => {
   cy.get('input[type="submit"][name="login"]').click();
 });
 
-Cypress.Commands.add('emptyForm', () => {
-    cy.get('input[type="submit"][name="login"]').click()
+Cypress.Commands.add('fillForm', (email, password) => {
+  cy.get('[id="username"]').type(email);
+  cy.get('[id="password"]').type(password);
 })
